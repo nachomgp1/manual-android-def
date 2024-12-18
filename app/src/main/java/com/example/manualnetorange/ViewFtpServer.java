afcac05537581ff012f2d6d8f566f6c8f6244cdf
nachomgp1 <ignaciopedrosar.sanjosemlg@fundacionloyola.net>
2024-12-18 09:45:05 +0100

    Ventana ftp server

---
+++ app/src/main/java/com/example/manualnetorange/ViewFtpServer.java
@@ -0,0 +1,47 @@
+package com.example.manualnetorange;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewFtpServer extends AppCompatActivity implements View.OnClickListener {
+
+RelativeLayout mainLayout;
+ImageButton btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ftp);
+
+        mainLayout = findViewById(R.id.mainLayoutFtp);
+
+        Intent intent = getIntent();
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setId(View.generateViewId());
+        btnBack.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == btnBack.getId()){
+            GoBack();
+        }
+    }
+
+    public void GoBack(){
+        Intent intent =new Intent(this, MainMenu.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/manualnetorange/ViewStandardUser.java
+++ app/src/main/java/com/example/manualnetorange/ViewStandardUser.java
@@ -2,14 +2,18 @@ package com.example.manualnetorange;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ViewStandardUser extends AppCompatActivity {
+public class ViewStandardUser extends AppCompatActivity implements View.OnClickListener {
 
     RelativeLayout mainLayout;
+    Button btnFtpServer;
+    Button btnEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +23,28 @@ public class ViewStandardUser extends AppCompatActivity {
 
         mainLayout = findViewById(R.id.mainStandarUserLayout);
 
+        btnFtpServer = findViewById(R.id.btnFtpServer);
+        btnFtpServer.setId(View.generateViewId());
+        btnFtpServer.setOnClickListener(this);
+
         Intent intent = getIntent();
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if(id == btnFtpServer.getId()){
+            OpenFtpServerView();
+        }
+    }
+
+    public void OpenFtpServerView(){
+        Intent intent = new Intent(ViewStandardUser.this, ViewFtpServer.class);
+        startActivity(intent);
+        finish();
 
     }
 }
