4917cafaf99be1efdfdafdeab684acbce4855232
samuel <samuelrobles12345@gmail.com>
2024-12-18 12:20:45 +0100

    Traducciones y algo mas

--- app/src/main/java/com/example/manualnetorange/ViewEmail.java
+++ app/src/main/java/com/example/manualnetorange/ViewEmail.java
@@ -2,15 +2,17 @@ package com.example.manualnetorange;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ViewEmail extends AppCompatActivity {
+public class ViewEmail extends AppCompatActivity implements View.OnClickListener{
 
     RelativeLayout mainLayout;
-
+    ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +20,22 @@ public class ViewEmail extends AppCompatActivity {
         setContentView(R.layout.activity_mail);
 
         Intent intent = getIntent();
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setId(View.generateViewId());
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == btnBack.getId()){
+            GoBack();
+        }
+    }
+
+    public void GoBack(){
+        Intent intent =new Intent(this, MainMenu.class);
+        startActivity(intent);
+        finish();
     }
 }
